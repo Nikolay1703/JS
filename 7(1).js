25ef815de083b0778d38917ef871ff65bbb09947
Nikolay1703 <cnm17@yandex.ru>
2019-01-22 20:55:04 +0300

    Task # 7(1)

---
+++ 7(1).js
@@ -0,0 +1,11 @@
+function range(start, end){
+    let array = [];
+    if (end = 10){
+        for (start = 1; start <= end; start++)
+        array.push(start);
+    }
+    return array;
+    }
+console.log(range(1, 10));
+
+
